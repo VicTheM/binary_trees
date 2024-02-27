d1e4b39deea37b16ae982674e9175b78ba3fc555
Nwofia Victory Chibuike <Victorychibuike121@gmail.com>
2024-02-27 08:26:49 +0100

    task 1 untested

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,1 +1,38 @@
+/* inserts a node as a left child to parent */
+#include "binary_trees.h"
 
+/**
+ * binary_tree_insert_left - inserts a left child
+ * @parent - parent node
+ * @value: value to be stored in left child
+ *
+ * NOTE: It also creates the node implicitly
+ *
+ * Return: pointer to created node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = value;
+	new->parent = parent;
+	new->right = NULL;
+
+	if (parent->left == NULL)
+	{
+		parent->left = new;
+		new->left = NULL;
+	}
+	else
+	{
+		new->left = parent->left;
+		parent->left = new;
+	}
+
+	return (new);
+}
